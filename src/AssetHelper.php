aca07eab2a74ba8af6b35a0d655b6d387242dd87
Raphael Barboza <eletronic.raphael@gmail.com>
2021-08-02 09:20:17 -0400

    AssetHelper

--- src/AssetHelper.php
+++ src/AssetHelper.php
@@ -8,11 +8,11 @@ class AssetHelper
 {
 
     public static function get($file){
-        return  config('files_url').'public/get/'.$file;
+        return  config('app.files_url').'public/get/'.$file;
     }
 
     public static function download($file){
-        return  config('files_url').'public/download/'.$file;
+        return  config('app.files_url').'public/download/'.$file;
     }
 
 }
